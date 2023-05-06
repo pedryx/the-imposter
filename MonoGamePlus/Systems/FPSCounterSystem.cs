2f50231e6278cfd61c4a37908198731dabc693d4
Petr Kotáb <petrkotab99@gmail.com>
2023-05-06 20:25:23 +0200

    FPS Counter.

---
+++ MonoGamePlus/Systems/FPSCounterSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace MonoGamePlus.Systems;
+public class FPSCounterSystem : GameSystem
+{
+    private readonly float[] times = new float[60];
+
+    private int index;
+
+    public float FPS { get; private set; }
+
+    protected override void Update(float elapsed)
+    {
+        times[index] = elapsed;
+
+        index++;
+        if (index >= times.Length)
+            index = 0;
+
+        FPS = 1.0f / times.Average();
+
+        Game.Window.Title = FPS.ToString();
+
+        base.Update(elapsed);
+    }
+}
